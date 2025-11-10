8d7a1ec6c35245b8a7f7a9adc480de4a7f7ca65a
Rahinur2002 <miahrahinur2002@gmail.com>
2025-11-10 13:54:51 -0500

    finished all the enu classes.

--- src/main/java/com/pluralsight/deli/common/enums/DrinkSize.java
+++ src/main/java/com/pluralsight/deli/common/enums/DrinkSize.java
@@ -1,5 +1,17 @@
 package com.pluralsight.deli.common.enums;
 
 public enum DrinkSize {
+    SMALL(12),
+    MEDIUM(16),
+    LARGE(20);
 
+    private final int ounces;
+
+    DrinkSize(int ounces) {
+        this.ounces = ounces;
+    }
+
+    public int getOunces() {
+        return ounces;
+    }
 }
