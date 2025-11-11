4b2c60e941d468820541d67f7fb0691ce6c2220f
Rahinur2002 <miahrahinur2002@gmail.com>
2025-11-11 18:57:56 -0500

    implemented product abstract class.

--- src/main/java/com/pluralsight/deli/products/sandwiches/toppings/premium/Cheese.java
+++ src/main/java/com/pluralsight/deli/products/sandwiches/toppings/premium/Cheese.java
@@ -1,4 +1,27 @@
 package com.pluralsight.deli.products.sandwiches.toppings.premium;
 
-public class Cheese {
+import com.pluralsight.deli.common.enums.SandwichSize;
+
+public class Cheese extends PremiumTopping{
+    public Cheese(String name, boolean hasExtra) {
+        super(name, hasExtra);
+    }
+
+    @Override
+    public double basePrice(SandwichSize s){
+        switch (s) {
+            case FOUR -> {
+                return 0.75;
+            }
+            case EIGHT -> {
+                return 1.50;
+            }
+            case TWELVE -> {
+                return 2.25;
+            }
+            default -> {
+                return 0;
+            }
+        }
+    }
 }
